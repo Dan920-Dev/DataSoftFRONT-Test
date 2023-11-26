d5e7700fa8c366c5ef6acdd3a90e8fffa1d56b2a
Dan920-Dev <00138218@uca.edu.sv>
2023-11-26 10:20:25 -0600

    Adding correct data to Modal book

--- src/components/BookModal.js
+++ src/components/BookModal.js
@@ -15,6 +15,8 @@ function BookModal({ book, show, onHide }) {
         <p className="text-muted">Precio: ${book.price}</p>
         {book.state && <div className={`badge ${book.state === 'DIS' ? 'bg-success' : 'bg-danger'}`}>
   {book.state === 'DIS' ? 'Disponible' : 'Ocupado'}</div>}
+  {book.user_name && <p className="Text-muted"> Alquilado por: 
+  {book.user_name === 'N/A' ? '' : book.user_name}</p>}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onHide}>Cerrar</Button>
